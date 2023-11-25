4e863f30dc3e6f8ef8e5f2f398510f9dab8805f5
mboulayeromien <62917611+mboulayeromien@users.noreply.github.com>
2023-11-25 16:02:45 +0100

    Update degProRec.py
    updated

--- degProRec.py
+++ degProRec.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pymongo
+
 
 st.title('UB Degree Program Recommender')
 st.header('DEGREE PROGRAM RECOMMENDATION SYSTEM FOR THE UNIVERSITY OF BUEA')
@@ -25,26 +25,3 @@ with st.form("my_form"):
         st.radio('Select Grade', options=grades, key=range(i, 100))
 
 
-@st.cache_resource
-def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
-
-
-client = init_connection()
-
-
-# Pull data from the collection.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def get_data():
-    db = client.mydb
-    items = db.mycollection.find()
-    items = list(items)  # make hashable for st.cache_data
-    return items
-
-
-items = get_data()
-
-# Print results.
-for item in items:
-    st.write(f"{item['name']} has a :{item['pet']}:")
