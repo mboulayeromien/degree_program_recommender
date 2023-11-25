598e358bc86e21b69d38ac35fd2b8454f494b584
mboulayeromien <romienmboulaye7@gmail.com>
2023-11-25 18:21:26 +0100

    better

--- degProRec.py
+++ degProRec.py
@@ -16,21 +16,20 @@ grades = ['A', 'B', 'C', 'D', 'E']
 students_subjects = []
 students_grades = []
 student_data = {}
+olevel = ['ENGLISH LANGUAGE', 'MATHEMATICS']
 with st.form("my_form"):
     submitted = st.form_submit_button("Submit")
     st.subheader('Advanced Level Subjects')
     no_subjects = int(st.number_input('How Many A Level Subjects Did You Pass In:'))
     for i in range(1, no_subjects + 1):
-        # if st.checkbox('Add Subject', key=i):
         students_subjects = st.selectbox('Select Subject Title', options=sorted(subjects), key=range(i, 200))
         students_grades = st.radio('Select Grade', options=grades, key=range(i, 100))
         student_data.update({students_subjects:students_grades})
     st.subheader('Ordinary Level Subjects')
-    english = st.write('ENGLISH LANGUAGE')
-    english_grade = st.radio('Select Grade', ['A', 'B', 'C'], key='english')
-    maths = st.write('MATHEMATICS')
-    maths_grade = st.radio('Select Grade', ['A', 'B', 'C'], key='maths')
-    student_data.update({english:english_grade, maths:maths_grade})
+    for i in range(1, 3):
+        olevel_sub = st.selectbox('Select Subject Title', olevel, key=range(i, 10))
+        olevel_grade = st.radio('Select Grade', ['A', 'B', 'C'], key=range(i, 30) )
+        student_data.update({olevel_sub:olevel_grade})
     
 if st.checkbox('Show Student Data'):
     st.write(student_data)
