b3a49d9cb8acf5b19031ff06d2e24ab951eb789b
mboulayeromien <romienmboulaye7@gmail.com>
2023-12-01 17:55:35 +0100

    new data

--- degProRec.py
+++ degProRec.py
@@ -74,7 +74,7 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 
 # Load the transaction data as a pandas dataframe
-df = pd.read_excel('prepared_alevel.xlsx')
+df = pd.read_csv('prepared_alevel_csv.csv')
 
 # Convert the dataframe into a binary matrix
 # df = df.pivot(index='user_id', columns='item_id', values='rating').fillna(0)
