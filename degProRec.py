06dec9b32871141d1482d8841c5501026e550777
mboulayeromien <romienmboulaye7@gmail.com>
2023-12-01 18:19:21 +0100

    gf

--- degProRec.py
+++ degProRec.py
@@ -62,6 +62,7 @@ from mlxtend.frequent_patterns import apriori, association_rules
 
 df = pd.read_csv('prepared_alevel_csv.csv')
 df = df.astype('bool')
+df = df.drop('Target', axis = 1)
 # Convert the dataframe into a binary matrix
 # df = df.pivot(index='user_id', columns='item_id', values='rating').fillna(0)
 df[df > 0] = 1
